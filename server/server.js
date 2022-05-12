2794b1f65a7bbe2f2317529d4adb7d4231ae4c02
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-05-12 00:23:44 -0400

    Added Jest testing basic framwork

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
-require('dotenv').config()
-const express = require('express');
-const server = express();
-
+const express = require('express')
+const server = express()
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 server.get('/', (req, res) => {
-  res.send('Hello Word!');
+  res.send('Hello Word, welcome to review service!');
 })
 
 server.listen(PORT, () => {
---
+++ tests/example.test.js
@@ -0,0 +1,5 @@
+describe('Some Example Tests', () => {
+  test('Expects true to be truthy', () => {
+    expect(true).toBeTruthy();
+  })
+});
