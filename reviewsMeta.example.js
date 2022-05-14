779bd74c1da8fa45a9a545f19f7c9be8627ea095
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-05-14 01:12:17 -0400

    Added reference for data shapes!

---
+++ reviewsMeta.example.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+var reviewMeta =
+{
+  "product_id": "71700",
+  "ratings": {
+      "2": "1",
+      "4": "1",
+      "5": "1"
+  },
+  "recommended": {
+      "false": "1",
+      "true": "2"
+  },
+  "characteristics": {  // Characteristics may not always exit!!
+      "Fit": {
+          "id": 240591,
+          "value": "3.6666666666666667"
+      },
+      "Length": {
+          "id": 240592,
+          "value": "3.6666666666666667"
+      },
+      "Comfort": {
+          "id": 240593,
+          "value": "3.6666666666666667"
+      },
+      "Quality": {
+          "id": 240594,
+          "value": "3.6666666666666667"
+      }
+  }
+}
