e257c24a8af3e58e53696021e1df46f67ea0326c
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-05-31 12:12:59 -0400

    Fixed Issue with model not updating by using await. Corrected counter and logging to ensure data updated every 10K entries

--- server/importDb/transform.js
+++ server/importDb/transform.js
@@ -14,29 +14,29 @@ const connectDB = async () => {
     await mongoose.connect(`mongodb://localhost:27017/${databaseName}`);
     console.log('MongoDB connected!!', Date());
 
-    var counter = 1;
+    var counter = 0;
     var counter2 = 0;
     for await (const review of Reviews.find()) {
 
-      //May need to make the code below promise based?
       var photos = await Photos.find({ review_id: review.id });
       var chars = await Characters.find({ review_id: review.id });
       try {
-        review.updateOne({
+        await review.updateOne({
           photos: photos,
           characteristics: chars,
-        }); //Might need to await this once... we'll see!
-
+        }); //MUST HAVE AWAIT, or else will not go through!
       } catch (e) {
-        console.log(e);
+        console.log('Error on update doc:', e);
+      }
+      counter++;
+      if (counter === 10000) {
+        counter2 += counter;
+        counter = 0;
+        console.log(counter2, Date());
+        var temp = await Reviews.find({ id: review.id })
+        console.log(temp[0]);
       }
 
-      counter++;
-      if (counter === 5000) {
-        counter2 += counter;
-        counter = 1;
-        console.log(counter2, Date());
-      }
     }
     counter2 += counter;
     console.log(counter2);
