db182039e90acf6ae499e8e7d99af2f9751a6bef
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-06-12 21:54:10 -0400

    WIP - written tests for DB saving on metas

--- tests/dbQueryTimes.test.js
+++ tests/dbQueryTimes.test.js
@@ -24,6 +24,31 @@ const reviewQueryTime = async (product_id) => {
   return [time, reviewList.length];
 };
 
+/**
+ *
+ * @param {Array} idList List of product_id's
+ * @param {Functions} asyncQueryCallBack db Query function MUST RETURN: `[time, numResults]`
+ * @returns `[averageTime, minTime, maxTime, [product_id, queryTime, numResults]]`
+ */
+const queryStats = async (idList, asyncQueryCallBack) => {
+  let minTime = 10000000000000000000;
+  let maxTime = -1;
+  let counter = 0;
+  let totalTime = 0;
+  let results = [];
+  for (let product_id of idList) {
+    let [queryTime, numResults] = await asyncQueryCallBack(product_id);
+    results.push([product_id, queryTime, numResults]);
+    totalTime += queryTime;
+    counter++;
+    minTime = queryTime < minTime ? queryTime : minTime;
+    maxTime = queryTime > maxTime ? queryTime : maxTime;
+  }
+  let averageTime = totalTime / counter;
+
+  return [averageTime, minTime, maxTime, results];
+};
+
 
 beforeAll(() => {
   return mongoose;
@@ -34,11 +59,12 @@ afterAll(async () => {
 });
 
 //------------------------------ Product Reviews ----------------------------------//
-describe('Times for Review Queries based on Product ID\'s should be <50 ms', () => {
+xdescribe('Times for Review Queries based on Product ID\'s should be <50 ms', () => {
   var productList = [];
   for (let i = 0; i < 50; i++) {
     productList.push(randProduct());
   }
+
   test('Query Reviews for 50 product_id: Times', async () => {
     let minTime = 10000000000000000000;
     let maxTime = -1;
@@ -67,15 +93,23 @@ describe('Times for Review Queries based on Product ID\'s should be <50 ms', () 
 const axios = require('Axios');
 
 /**
- * Ensure that server is up and running
+ * Please ensure server is up and running first
  * @param {Number} product_id
  * @param {String} hostURL URL for the localhost and port. Example: http://localhost:3001
+ * @returns `[queryTime, response.status, response.statusText]`
  */
 const metaConstuctTime = async (product_id, hostURL = 'http://localhost:3001') => {
-  axios({
-    method: 'get',
-    url: `${hostURL}/reviews/meta?product_id=${product_id}`
-  })
+  let start = Date.now();
+  try {
+    var response = await axios({
+      method: 'get',
+      url: `${hostURL}/reviews/meta?product_id=${product_id}`
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  let time = Date.now() - start;
+  return [time, response.status, response.statusText];
 };
 
 const metaQueryTime = async (product_id) => {
@@ -90,11 +124,26 @@ const metaQueryTime = async (product_id) => {
   return [time, reviewMeta.length];
 };
 
-xdescribe('Times for ReviewMeta Queries based on Product ID\'s should be <50 ms', () => {
+describe('Times for ReviewMetas Tests', () => {
   var productList = [];
   for (let i = 0; i < 50; i++) {
     productList.push(randProduct());
   }
+
+  // Resets ReviewMetas Collection
+  beforeAll(async () => {
+    await ReviewMetas.collection.drop();
+    await ReviewMetas.init();
+    return;
+  });
+
+  test('Request Server to Return ReviewMetas', async () => {
+    let [averageTime, minTime, maxTime, results] = await queryStats(productList, metaConstuctTime);
+    console.log(`Average Time: ${averageTime},  Min Time: ${minTime},  Max Time: ${maxTime}`);
+    console.log('Results: product_id / queryTime [ms] / MetaReturned \n', results);
+  });
+
+
   test('Query ReviewMetas for 50 product ID Times', async () => {
     let minTime = 10000000000000000000;
     let maxTime = -1;
