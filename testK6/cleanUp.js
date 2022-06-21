c153248a793004946be737c709702ad44e121f13
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-06-20 23:00:28 -0400

    added testin clean up scripts

---
+++ testK6/cleanUp.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const databaseName = 'atelierReviews';
+const {
+  Reviews,
+  ReviewIncrementer
+} = require('../db/schemas.postELT.js');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`mongodb://localhost:27017/${databaseName}`);
+    console.log('MongoDB connected!: Cleaning up After testing', Date());
+
+    // Deleting added reviews
+    try {
+      await Reviews.deleteMany({ review_id: { $gt: 5774952 } })
+    } catch (e) {
+      console.log(e.message)
+    }
+
+    // set all reviews to not reported
+    try {
+      await Reviews.updateMany({},{ $set: { reported: false }})
+    } catch (e) {
+      console.log(e.message)
+    }
+
+    // resetting the incrementer for the review_id
+    try {
+      await ReviewIncrementer.collection.drop();
+    } catch (e) {
+      console.log(e.message)
+    }
+
+    var [{ review_id }] = await Reviews.find({}).sort({ _id: -1 }).limit(1); //returns an array
+    console.log('Last Inserted review_id', review_id); // New Db Should be 5774952
+    var incrementer = await ReviewIncrementer.create({ review_id: review_id + 1 });
+    console.log('Next reivew_id will be', incrementer.review_id);
+
+    console.log('Clean up completed', Date());
+
+  } catch (err) {
+    console.log('Failed to connect to MongoDB', err);
+
+  }
+  await mongoose.connection.close();
+};
+
+connectDB();
+
