1ccfd9d4babbcd02948f93ce9c0c1f4f6bcaf114
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-06-24 01:15:52 -0400

    WIP Setting up Remote connection to DB only EC2 instance

--- initServer.sh
+++ initServer.sh
@@ -1,3 +1,2 @@
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3001
-sudo systemctl start mongod
 node server/server.js
---
+++ mongoRemote.sh
@@ -0,0 +1,1 @@
+mongosh --host <IP address here> --port 27017 -u server -p --authenticationDatabase atelierReviews
