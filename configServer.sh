e4c6e0edb1e349ca95231184733028a80f7c8c48
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-06-25 15:09:50 -0400

    Adding mongosh installation to the server

--- configServer.sh
+++ configServer.sh
@@ -2,8 +2,12 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install git
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash;
-bash ~/.nvm/nvm.sh
+. ~/.nvm/nvm.sh
 nvm install --lts
 node -e "console.log('Running Node.js ' + process.version)"
 git config --global alias.ci commit
 git config --global alias.st status
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-mongosh
