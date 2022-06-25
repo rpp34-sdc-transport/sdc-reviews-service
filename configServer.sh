62b6142271373d7750b873e79d8c17397008c3c3
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-06-25 14:50:58 -0400

    minor script fix

--- configServer.sh
+++ configServer.sh
@@ -1,8 +1,8 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install git
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-. ~/.nvm/nvm.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash;
+bash ~/.nvm/nvm.sh
 nvm install --lts
 node -e "console.log('Running Node.js ' + process.version)"
 git config --global alias.ci commit
