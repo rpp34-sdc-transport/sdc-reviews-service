a3d0a5a58bbadcded0ba669d98492b138bbb1d75
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-06-25 15:56:22 -0400

    Connected server to EC2 Standlong DB

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
-const databaseName = 'atelierReviews'
-mongoose.connect(`mongodb://localhost:27017/${databaseName}`,
+require('dotenv').config();
+const databaseName = process.env.MONGO_DB
+mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_TOKEN}@${process.env.MONGO_HOST}:27017/${databaseName}?authSource=${databaseName}`,
+  {
+
+  },
   () => console.log(`Connected to ${databaseName}`),
   (err) => console.log(err)
 );
