99f59146f67c4700c497207da5cabb20a1ad99ea
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-06-25 14:31:11 -0400

    Adding bash script for setting up initial server instance

---
+++ configServer.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 16
+node -e "console.log('Running Node.js ' + process.version)"
+git config --global alias.ci commit
+git config --global alias.st status
