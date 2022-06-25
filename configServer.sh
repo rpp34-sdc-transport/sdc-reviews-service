a3721acecc7c7c7ba1711d064bd3c3d01be0cfbc
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-06-25 14:47:58 -0400

    Changing npm install

--- configServer.sh
+++ configServer.sh
@@ -3,7 +3,7 @@ sudo apt-get upgrade -y
 sudo apt-get install git
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install 16
+nvm install --lts
 node -e "console.log('Running Node.js ' + process.version)"
 git config --global alias.ci commit
 git config --global alias.st status
