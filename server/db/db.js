1e90cebc531a0a8c01d5c9945ad707152341b571
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-06-25 17:31:49 -0400

    Server still uses non admin login

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const databaseName = process.env.MONGO_DB
-const authDB = process.env.MONGO_AUTHDB;
-mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_TOKEN}@${process.env.MONGO_HOST}:27017/${databaseName}?authSource=${authDB}`,
+mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_TOKEN}@${process.env.MONGO_HOST}:27017/${databaseName}?authSource=${databaseName}`,
   {},
   () => console.log(`Connected to ${databaseName}`),
   (err) => console.log(err)
