56a0434e06566373f64209a278d13e60df1650bf
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-06-26 19:01:32 -0400

    updated bash scripts

--- bashScripts/configServer.sh
+++ bashScripts/configServer.sh
@@ -1,8 +1,8 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash;
-. ~/.nvm/nvm.sh
-nvm install --lts
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash;
+sudo bash ~/.nvm/nvm.sh
+sodo nvm install --lts
 node -e "console.log('Running Node.js ' + process.version)"
 git config --global alias.ci commit
 git config --global alias.st status
