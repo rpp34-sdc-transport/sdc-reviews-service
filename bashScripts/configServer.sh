4bbc66815ecfb546d34989fe9836bb4b76097673
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-06-30 19:53:07 -0400

    Combining bash scripts into one for config-server

--- bashScripts/configServer.sh
+++ bashScripts/configServer.sh
@@ -9,5 +9,13 @@ sudo apt-get update
 sudo apt-get install -y mongodb-mongosh
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash;
 source ~/.bashrc
-nvm install --lts
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 16.15.1
 node -e "console.log('Running Node.js ' + process.version)"
+npm install pm2@latest -g
+pm2 start server/server.js
+sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v16.15.1/bin /home/ubuntu/.nvm/versions/node/v16.15.1/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+pm2 save
+pm2 status
