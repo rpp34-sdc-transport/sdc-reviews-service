92978b459d4763ca91f1efddf4f50f47a343339d
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-06-30 19:37:44 -0400

    adding pm2 config bash script

--- bashScripts/configServer2.sh
+++ bashScripts/configServer2.sh
@@ -1,2 +1,7 @@
-nvm install --lts
+nvm install node 16.15.1
 node -e "console.log('Running Node.js ' + process.version)"
+npm install pm2@latest -g
+pm2 start server/server.js
+sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v16.15.1/bin /home/ubuntu/.nvm/versions/node/v16.15.1/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+pm2 save
+pm2 status
